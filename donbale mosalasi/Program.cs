a90da40772d7065227d8fdf47acc30522ac8f5df
arminlry <arminlry@DESKTOP-H8CJ8C9>
2023-10-31 22:07:53 +0330

    Add project files.

---
+++ donbale mosalasi/Program.cs
@@ -0,0 +1,31 @@
+﻿// armin liryaee
+// triangular sequence
+// tamrin 6
+ char a = ' ';
+do
+{
+
+    Console.WriteLine("enter n number:");
+
+    long n = Convert.ToInt64(Console.ReadLine());
+
+   
+    Console.WriteLine("enter m number");
+
+    long m = Convert.ToInt64(Console.ReadLine());
+
+
+    for (long t = n; t <= m; t++)
+{           
+        long o = t * (t + 1) / 2;
+
+       
+        Console.WriteLine(a);
+
+        Console.WriteLine(o);
+
+
+    }
+
+    Console.WriteLine("do you want to continue?");
+} while(Console.ReadLine() == "y");
